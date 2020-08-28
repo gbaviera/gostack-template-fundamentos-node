5a3e9a1fae6e7bceb304ad8d922771158ead9fad
Guilherme Baviera <guilhermebaviera@gmail.com>
2020-08-27 21:02:18 -0300

    Implementando o desafio 05 do goStack

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,32 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: RequestDTO): Transaction {
+    // type !== 'income' && type !== 'outcome'
+    if (!['income', 'outcome'].includes(type)) {
+      throw Error('Type must be income or outcome.');
+    }
+
+    if (typeof value !== 'number') {
+      throw Error('Value must be a number.');
+    }
+
+    if (typeof title !== 'string') {
+      throw Error('Title must be a string.');
+    }
+
+    const { total } = this.transactionsRepository.getBalance();
+    if (type === 'outcome' && value > total) {
+      throw Error("You don't have enought balance.");
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
